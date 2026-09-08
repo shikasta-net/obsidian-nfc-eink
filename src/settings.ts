4c13549311f0d451f2f5bde35756e0234c8f00be
Kym Eden <shikasta.net@gmail.com>
2026-09-08 06:36:24 +0100

    Initial commit

---
+++ src/settings.ts
@@ -0,0 +1,42 @@
+import {
+  PluginSettingTab,
+  Setting,
+  type App,
+} from 'obsidian';
+import NfcEinkPlugin from './main';
+
+export interface NfcEinkPluginSettings {
+  mySetting: string;
+}
+
+export const DEFAULT_SETTINGS: NfcEinkPluginSettings = {
+  mySetting: 'default',
+};
+
+export class NfcEinkSettingsTab extends PluginSettingTab {
+  plugin: NfcEinkPlugin;
+
+  constructor(app: App, plugin: NfcEinkPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display(): void {
+    const { containerEl } = this;
+
+    containerEl.empty();
+
+    new Setting(containerEl)
+      .setName('Settings #1')
+      .setDesc("It's a secret")
+      .addText((text) =>
+        text
+          .setPlaceholder('Enter your secret')
+          .setValue(this.plugin.settings.mySetting)
+          .onChange(async (value) => {
+            this.plugin.settings.mySetting = value;
+            await this.plugin.saveSettings();
+          }),
+      );
+  }
+}
